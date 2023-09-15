fa35560f05fc1a8aa110a58613e628b785d84b86
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-15 21:27:07 +0300

    task 5 hw-6

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const textInput = document.querySelector('#name-input');
+
+const textOutput = document.querySelector('#name-output');
+
+const inputHandle = event => {
+    textOutput.textContent = event.currentTarget.value;
+    if (!textInput.value) {
+        textOutput.textContent = "Anonymous";
+    }
+}
+
+textInput.addEventListener('input', inputHandle);
