c9abe43c6547b632ea40c94fdf1281176e5acf34
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-15 17:19:03 +0300

    task 2 hw-6

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+let newArray = [];
+
+const newIngredients = ingredients.forEach(ingredient => {
+  const ingr = document.createElement('li');
+  ingr.textContent = ingredient;
+  ingr.classList.add('ingredients-item');
+  newArray.push(ingr);
+});
+
+ingredientsList.append(...newArray);
