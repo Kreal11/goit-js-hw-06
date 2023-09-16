2c2427b6e6840ad201faf7756540993b02dc000e
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-16 15:17:14 +0300

    updated 5 n 6 task, done 7th task

--- js/task-05.js
+++ js/task-05.js
@@ -3,6 +3,7 @@ const textInput = document.querySelector('#name-input');
 const textOutput = document.querySelector('#name-output');
 
 const inputHandle = event => {
+    event.preventDefault();
     textOutput.textContent = event.currentTarget.value;
     if (!textInput.value) {
         textOutput.textContent = "Anonymous";
--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('#validation-input');
 
 const checkInput = (event) => {
-
+    event.preventDefault();
     if (event.currentTarget.value.length === 6) {
         input.classList.add('valid')
         input.classList.remove('invalid')
