1ac47e48eb54aacb50ee1d8868672f7f7a625332
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-17 15:30:55 +0300

    task 10 hw-6

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input[type = number]');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const divCollection = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', createBoxes)
+btnDestroy.addEventListener('click', destroyBoxes)
+
+function createBoxes() {
+  const amount = Number(input.value);
+  let boxSize = 30;
+  let newDivArray = [];
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    divCollection.append(box);
+    boxSize += 10;
+  }
+  // const divs = newDivArray.map(div => div).join('');
+  // divCollection.insertAdjacentHTML('afterbegin', divs);
+}
+
+function destroyBoxes() {
+  divCollection.textContent = '';
+}
+
