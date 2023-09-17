a35f7f8b510bfe25cea6b436b9bc6ed1fe24c137
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-17 15:38:30 +0300

    update task 10 hw-6

--- js/task-10.js
+++ js/task-10.js
@@ -15,7 +15,7 @@ btnDestroy.addEventListener('click', destroyBoxes)
 function createBoxes() {
   const amount = Number(input.value);
   let boxSize = 30;
-  let newDivArray = [];
+  // let newDivArray = [];
   for (let i = 0; i < amount; i += 1) {
     const box = document.createElement('div');
     box.style.backgroundColor = getRandomHexColor();
